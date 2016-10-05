eddb511f846dd28cf8565f2318becea9ddcb438f
Giulia Di Bella <dibellagiulia@gmail.com>
2016-10-05 16:46:12 +0200

    Main Activity, Login Activity added

---
+++ app/src/main/java/it/unipi/iet/onspot/MainActivity.java
@@ -0,0 +1,24 @@
+package it.unipi.iet.onspot;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button SignUp = (Button) findViewById(R.id.button);
+        SignUp.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
