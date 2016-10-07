93383de5020e16afab53dead4cfab4cd283ae062
Giulia Di Bella <dibellagiulia@gmail.com>
2016-10-07 12:01:45 +0200

    LoginActivity Modified and ProfileActivity Added

--- app/src/main/java/it/unipi/iet/onspot/LoginActivity.java
+++ app/src/main/java/it/unipi/iet/onspot/LoginActivity.java
@@ -1,46 +1,22 @@
 package it.unipi.iet.onspot;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
@@ -52,12 +28,16 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener mAuthListener;
 
     private String TAG = "LoginActivity";
+    private  EditText emailField;
+    private EditText passwordField;
+    private Button Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Login = (Button)findViewById(R.id.email_sign_in_button);
         // auth initialization
         mAuth = FirebaseAuth.getInstance();
 
@@ -79,19 +59,60 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    public void signin(View v){
+    // Create a new account by passing the new user's email address and password
+    public void create_new_account(String email, String password){
 
-        EditText email = (EditText)findViewById(R.id.email);
-        EditText password = (EditText)findViewById(R.id.password);
+        //check format email and password
+        if(!validate_form(email,password))
+            return;
 
-        //TODO: check format email and password
-
-        // [START sign_in_with_email]
-        mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+        //TODO: check if it works and start profile activity
+        /*
+        mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });*/
+
+
+    }
+
+    // Signing in a user with a password
+    public void sign_in(String email,String password){
+
+        //check format email and password
+        if(!validate_form(email,password))
+            return;
+
+        Login.setEnabled(false);
+        // Show screen spinner while authenticating
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
                         Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        Intent i = new Intent(LoginActivity.this,MapsActivity.class);
+                        startActivity(i);
+
 
                         // If sign in fails, display a message to the user. If sign in succeeds
                         // the auth state listener will be notified and logic to handle the
@@ -100,10 +121,47 @@ public class LoginActivity extends AppCompatActivity {
                             Log.w(TAG, "signInWithEmail:failed", task.getException());
                             Toast.makeText(LoginActivity.this, R.string.auth_failed,
                                     Toast.LENGTH_SHORT).show();
+                            Login.setEnabled(true);
                         }
                     }
                 });
         // [END sign_in_with_email]
+    }
+
+    public void onClick(View v){
+
+       emailField = (EditText)findViewById(R.id.email);
+       passwordField = (EditText)findViewById(R.id.password);
+
+        Intent intent = getIntent();
+        String butt = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // if sign_in or sign up button was clicked
+        if(butt.equals("button")){
+            sign_in(emailField.getText().toString(),passwordField.getText().toString());
+        } else if(butt.equals("button2")){
+            create_new_account(emailField.getText().toString(),passwordField.getText().toString());
+        }
+    }
+
+    // Check if form's fields are compiled properly
+    public boolean validate_form(String email, String password){
+        boolean valid = true;
+        if(TextUtils.isEmpty(email)|| !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailField.setError("Enter a valid email address");
+            valid = false;
+        }else{
+            emailField.setError(null);
+        }
+
+        if(TextUtils.isEmpty(password) || password.length() < 4 || password.length() > 10){
+            passwordField.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        }else{
+            passwordField.setError(null);
+        }
+
+        return valid;
     }
 
     @Override
--- app/src/main/java/it/unipi/iet/onspot/MainActivity.java
+++ app/src/main/java/it/unipi/iet/onspot/MainActivity.java
@@ -8,15 +8,29 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity  {
 
+    static final String EXTRA_MESSAGE = "it.unipi.iet.onspot.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button SignUp = (Button) findViewById(R.id.button);
+        // Sign in Button
+        final Button SignIn = (Button) findViewById(R.id.button);
+        SignIn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                intent.putExtra(EXTRA_MESSAGE,"button");
+                startActivity(intent);
+            }
+        });
+
+        // Sign up with email Button
+        final Button SignUp = (Button) findViewById(R.id.button2);
         SignUp.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                intent.putExtra(EXTRA_MESSAGE,"button2");
                 startActivity(intent);
             }
         });
