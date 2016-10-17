073a5579ac98b5499328ee97904edc7fdd91abe8
ericafaraoni <e.faraoni@hotmail.it>
2016-10-17 18:03:06 +0200

    Modify MapsActivity

--- app/src/main/java/it/unipi/iet/onspot/MapsActivity.java
+++ app/src/main/java/it/unipi/iet/onspot/MapsActivity.java
@@ -37,20 +37,30 @@ import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnMenuTabSelectedListener;
 
 
-public class MapsActivity extends AppCompatActivity implements ConnectionCallbacks, OnConnectionFailedListener, OnMapReadyCallback, OnMyLocationButtonClickListener, ActivityCompat.OnRequestPermissionsResultCallback  {
+public class MapsActivity extends AppCompatActivity implements ConnectionCallbacks, OnConnectionFailedListener, OnMapReadyCallback, OnMyLocationButtonClickListener, ActivityCompat.OnRequestPermissionsResultCallback, LocationListener  {
 
         private GoogleMap mMap;
+
         //Request code for location permission request.
         private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
         //Flag indicating whether a requested permission has been denied after returning in.
         private boolean mPermissionDenied = false;
 
+        // Keys for storing activity state in the Bundle.
+        protected final static String LOCATION_KEY = "location-key";
+        //The desired interval for location updates. Inexact. Updates may be more or less frequent.
+        public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+        //The fastest rate for active location updates. Exact. Updates will never be more frequent than this value.
+        public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+        //Tracks the status of the location updates request. Value changes when the user presses the Start Updates and Stop Updates buttons.
+        protected Boolean mRequestingLocationUpdates;
+
         //Provides the entry point to Google Play services.
         protected GoogleApiClient mGoogleApiClient;
-
+        //Stores parameters for requests to the FusedLocationProviderApi.
+        protected LocationRequest mLocationRequest;
         //Represents a geographical location.
-        protected Location mLastLocation;
-
+        protected Location mCurrentLocation;
 
         // variabili di firebase
         private FirebaseAuth mAuth;
@@ -62,6 +72,8 @@ public class MapsActivity extends AppCompatActivity implements ConnectionCallbac
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_maps);
 
+            mRequestingLocationUpdates = true;
+
             // Obtain the SupportMapFragment and get notified when the map is ready to be used.
             SupportMapFragment mapFragment = SupportMapFragment.newInstance();
             FragmentTransaction fragTransaction = getSupportFragmentManager().beginTransaction();
@@ -69,6 +81,9 @@ public class MapsActivity extends AppCompatActivity implements ConnectionCallbac
             fragTransaction.commit();
             mapFragment.getMapAsync(this);
 
+            // Update values using data stored in the Bundle.
+            updateValuesFromBundle(savedInstanceState);
+            // Kick off the process of building a GoogleApiClient and requesting the LocationServices API.
             buildGoogleApiClient();
 
             /*
@@ -111,60 +126,132 @@ public class MapsActivity extends AppCompatActivity implements ConnectionCallbac
 
         }
 
-    /**
-     * Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
-     */
-    protected synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
-
-    /**
-     * Runs when a GoogleApiClient object successfully connects.
-     */
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        // Provides a simple way of getting a device's location and is well suited for
-        // applications that do not require a fine-grained location and that do not need location
-        // updates. Gets the best and most recent location currently available, which may be null
-        // in rare cases when a location is not available.
-        Log.d(TAG, "entrato in on connected");
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        Log.d(TAG, "esco da on connected");
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
-        // onConnectionFailed.
-        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
-    }
-
-
-    @Override
-    public void onConnectionSuspended(int cause) {
-        // The connection to Google Play services was lost for some reason. We call connect() to
-        // attempt to re-establish the connection.
-        Log.i(TAG, "Connection suspended");
-        mGoogleApiClient.connect();
-    }
-
         /*
-         *  Google Maps functions
-         */
+        *  Google Maps functions
+        */
+
+        //Updates fields based on data stored in the bundle.
+        private void updateValuesFromBundle(Bundle savedInstanceState) {
+            Log.i(TAG, "Updating values from bundle");
+            if (savedInstanceState != null) {
+                // Update the value of mCurrentLocation from the Bundle
+                if (savedInstanceState.keySet().contains(LOCATION_KEY)) {
+                    // Since LOCATION_KEY was found in the Bundle, we can be sure that mCurrentLocation
+                    // is not null.
+                    mCurrentLocation = savedInstanceState.getParcelable(LOCATION_KEY);
+                }
+            }
+        }
+
+        //Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
+        protected synchronized void buildGoogleApiClient() {
+            Log.i(TAG, "Building GoogleApiClient");
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+            createLocationRequest();
+        }
+
+        //Sets up the location request.
+        protected void createLocationRequest() {
+            mLocationRequest = new LocationRequest();
+
+            // Sets the desired interval for active location updates.
+            mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+
+            // Sets the fastest rate for active location updates.
+            mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+
+            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        }
+
+        //Requests location updates from the FusedLocationApi.
+        protected void startLocationUpdates() {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+
+        //Removes location updates from the FusedLocationApi.
+        protected void stopLocationUpdates() {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+
+        // Stop location updates to save battery, but don't disconnect the GoogleApiClient object.
+        @Override
+        protected void onPause() {
+            super.onPause();
+            if (mGoogleApiClient.isConnected()) {
+                stopLocationUpdates();
+            }
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            if (mGoogleApiClient.isConnected() && mRequestingLocationUpdates) {
+                startLocationUpdates();
+            }
+        }
+
+        //Runs when a GoogleApiClient object successfully connects.
+        @Override
+        public void onConnected(Bundle connectionHint) {
+            // If the initial location was never previously requested, we use
+            // FusedLocationApi.getLastLocation() to get it. If it was previously requested, we store
+            // its value in the Bundle and check for it in onCreate(). We
+            // do not request it again unless the user specifically requests location updates by pressing
+            // the Start Updates button.
+            //
+            // Because we cache the value of the initial location in the Bundle, it means that if the
+            // user launches the activity,
+            // moves to a new location, and then changes the device orientation, the original location
+            // is displayed as the activity is re-created.
+            if (mCurrentLocation == null) {
+                mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            }
+
+            startLocationUpdates();
+        }
+
+        //Callback that fires when the location changes.
+        @Override
+        public void onLocationChanged(Location location) {
+            mCurrentLocation = location;
+            Toast.makeText(this, getResources().getString(R.string.location_updated_message),
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onConnectionFailed(ConnectionResult result) {
+            Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+        }
+
+
+        @Override
+        public void onConnectionSuspended(int cause) {
+            // The connection to Google Play services was lost for some reason. We call connect() to
+            // attempt to re-establish the connection.
+            Log.i(TAG, "Connection suspended");
+            mGoogleApiClient.connect();
+        }
+
+        //Stores activity data in the Bundle.
+        public void onSaveInstanceState(Bundle savedInstanceState) {
+            savedInstanceState.putParcelable(LOCATION_KEY, mCurrentLocation);
+            super.onSaveInstanceState(savedInstanceState);
+        }
 
         //Manipulates the map once available.
         @Override
         public void onMapReady(GoogleMap googleMap) {
             mMap = googleMap;
+            enableMyLocation();
             LatLng pos;
             Log.d(TAG, "entrato in map ready");
 
-            if (mLastLocation != null) {
-                pos = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+            if (mCurrentLocation != null) {
+                pos = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
                 Log.d(TAG, "location ottenuta");
             } else {
                 Log.d(TAG, "location nulla");
@@ -174,9 +261,7 @@ public class MapsActivity extends AppCompatActivity implements ConnectionCallbac
             // Add a marker in the current position and move the camera
             mMap.moveCamera(CameraUpdateFactory.newLatLng(pos));
             mMap.animateCamera(CameraUpdateFactory.zoomTo(20), 2000, null);
-
             mMap.setOnMyLocationButtonClickListener(this);
-            enableMyLocation();
         }
 
         //Enables the My Location layer if the fine location permission has been granted.
