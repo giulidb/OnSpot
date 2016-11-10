ef5405502209780204ed5cd653be1a8b315c99e4
ericafaraoni <e.faraoni@hotmail.it>
2016-11-10 11:28:17 +0100

    Search fragment style changed

--- app/src/main/java/it/unipi/iet/onspot/fragments/SearchFragment.java
+++ app/src/main/java/it/unipi/iet/onspot/fragments/SearchFragment.java
@@ -1,11 +1,16 @@
 package it.unipi.iet.onspot.fragments;
 
 
+import android.content.res.TypedArray;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 
 import it.unipi.iet.onspot.R;
@@ -15,12 +20,45 @@ public class SearchFragment extends DialogFragment {
 
     private EditText locationSearch;
 
+    public SearchFragment() {
+        // Empty constructor is required for DialogFragment
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NO_TITLE, 0);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
         locationSearch = (EditText) view.findViewById(R.id.search_text);
+
         return view;
+    }
+
+    @Override
+    public void onResume() {
+
+        // Change dialog style
+        Window window = getDialog().getWindow();
+        window.setGravity(Gravity.TOP);
+        ViewGroup.LayoutParams params = window.getAttributes();
+        params.width = WindowManager.LayoutParams.MATCH_PARENT;
+        params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        window.setAttributes((android.view.WindowManager.LayoutParams) params);
+        final TypedArray styledAttributes = getContext().getTheme().obtainStyledAttributes(
+                new int[] { android.R.attr.actionBarSize });
+        int mActionBarSize = (int) styledAttributes.getDimension(0, 0);
+        //styledAttributes.recycle();
+        WindowManager.LayoutParams par = window.getAttributes();
+        par.y = -mActionBarSize;
+        window.setAttributes(par);
+
+        // Call super onResume after sizing
+        super.onResume();
     }
 
     public String getLocation() {
